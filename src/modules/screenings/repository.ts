e8e8ed21df0d157e0205898fb8124cf44d73d9f0
Akvile Jankauskaite <akvile.jankauskait@gmail.com>
2023-11-12 14:03:49 +0200

    Update repository and screenings tests

--- src/modules/screenings/repository.ts
+++ src/modules/screenings/repository.ts
@@ -1,12 +1,12 @@
 import type { Database } from '@/database'
-import type { Screenings } from '@/database'
+//import type { Screenings } from '@/database'
 
 
-export default (db: Database) => ({
+ export default (db: Database) => ({
   findAll: async (limit = 10, offset = 0) =>
     db.selectFrom('screenings').selectAll().limit(limit).offset(offset).execute(),
 
-  // findByIds: async (ids: number[]) =>
-  //   db.selectFrom('screenings').selectAll().where('id', 'in', ids).execute(),
-})
+//   // findByIds: async (ids: number[]) =>
+//   //   db.selectFrom('screenings').selectAll().where('id', 'in', ids).execute(),
+ })
 
--- src/modules/screenings/tests/screenings.spec.ts
+++ src/modules/screenings/tests/screenings.spec.ts
@@ -1,73 +1,55 @@
 import supertest from 'supertest';
-// import createTestDatabase from '@tests/utils/createTestDatabase'
-import createDatabase from '@/database';
+import createTestDatabase from '@tests/utils/createTestDatabase';
+import { createFor } from '@tests/utils/records';
 import createApp from '@/app';
+// import createDatabase from '@/database';
 
-// Testing with a real database, fine for read-only tests, as we would not
-// want to pollute the database with test data, as then we need to clean it up.
-// To make sure we are not modifying anything in a real database, we are using
-// a read-only connection.
-const db = createDatabase(process.env.DATABASE_URL as string, {
-  readonly: true,
-});
+// const db = createDatabase(process.env.DATABASE_URL as string, {
+//   readonly: true,
+// });
 
-// We could also easily use an in-memory database here, but then we would need
-// to provide some test data
-// const db = await createTestDatabase()
-
+const db = await createTestDatabase();
 const app = createApp(db);
+const createScreenings = createFor(db, 'screenings');
+const createMovies = createFor(db, 'movies');
 
 describe('GET', () => {
-  it('should return movies by a list of query params', async () => {
-    const { body } = await supertest(app)
-      .get('/movies?id=133093,816692')
-      .expect(200);
-
-    expect(body).toHaveLength(2);
-    expect(body).toEqual([
-      {
-        id: 133093,
-        title: 'The Matrix',
-        year: 1999,
-      },
+  it('should return all screenings', async () => {
+    const movieTest = [
       {
         id: 816692,
         title: 'Interstellar',
         year: 2014,
       },
-    ]);
-  });
+    ];
+    const timestamp = new Date().toISOString();
+    const screeningTest = [
+      {
+        timestamp: '2023-11-01T21:15:00.0000Z',
+        movieId: 816692,
+        createdAt: timestamp,
+        updatedAt: timestamp,
+      },
+    ];
 
-  it('should return a BadRequest error if query params are missing', async () => {
-    const { body } = await supertest(app).get('/movies').expect(400);
+    await createMovies(movieTest);
+    await createScreenings(screeningTest);
 
-    expect(body.error.message).toBe('Movies ids are required.');
-  });
+    const { body } = await supertest(app).get('/screenings').expect(200);
 
-  it('should return a BadRequest error if id query params are not numbers', async () => {
-    const { body } = await supertest(app).get('/movies?id=one,two').expect(400);
-
-    expect(body.error.issues[0].message).toBe(
-      'Movies ids must be of numeric type.'
-    );
-    expect(body.error.name).toBe('ZodError');
-  });
-
-  it('should return error message if movie is not found in the database', async () => {
-    const { body } = await supertest(app).get('/movies?id=1').expect(404);
-    expect(body.error.message).toBe('No movies can be found in the database');
-  });
-
-  it('should return movies that are found in the database', async () => {
-    const { body } = await supertest(app)
-      .get('/movies?id=1,816692')
-      .expect(200);
-    expect(body).toHaveLength(1);
     expect(body).toEqual([
       {
-        id: 816692,
-        title: 'Interstellar',
-        year: 2014,
+        movieId: 816692,
+        movieTitle: 'Interstellar',
+        movieYear: 2014,
+        screenings: [
+          {
+            id: 1,
+            timestamp: '2023-11-06T21:15:00.0000Z',
+            totalTickets: 100,
+            ticketsLeft: 100,
+          },
+        ],
       },
     ]);
   });
